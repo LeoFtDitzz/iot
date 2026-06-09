201ad6c92c67372ab1baa87b1dfaa92cdfef4417
LeoFtDitzz <leoftradit@gmail.com>
2026-06-09 14:25:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,49 +4,63 @@ app = Flask(__name__)
 
 data_sensor = {
     "suhu": 0,
-    "cahaya": 0
+    "cahaya": 0,
+    "kondisi": "-",
+    "waktu": "-",
+    "lampu": "OFF"
 }
+
+command = "AUTO"
+
 
 @app.route("/")
 def home():
-    return "Hello"
+    return "IoT Server Running"
+
 
 @app.route("/data", methods=["POST"])
 def receive_data():
-global data_sensor
 
-```
-data_sensor = request.json
+    global data_sensor
 
-return jsonify({
-    "status": "success"
-})
-```
+    data_sensor = request.json
+
+    print(data_sensor)
+
+    return jsonify({
+        "status": "success"
+    })
+
 
 @app.route("/sensor")
 def sensor():
-return jsonify(data_sensor)
+
+    return jsonify(data_sensor)
+
 
 @app.route("/command")
 def get_command():
-return command
+
+    return command
+
 
 @app.route("/set_command/<cmd>")
 def set_command(cmd):
-global command
 
-```
-command = cmd.upper()
+    global command
 
-return jsonify({
-    "status": "success",
-    "command": command
-})
-```
+    command = cmd.upper()
 
-if **name** == "**main**":
-app.run(
-host="0.0.0.0",
-port=5000,
-debug=True
-)
+    return jsonify({
+        "status": "success",
+        "command": command
+    })
+
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )

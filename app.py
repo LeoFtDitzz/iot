72237bf326143bd522d3b80351908d6fc896a7c4
LeoFtDitzz <leoftradit@gmail.com>
2026-06-09 14:03:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,74 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(**name**)
 
 data_sensor = {
-    "suhu": 0,
-    "cahaya": 0,
-    "kondisi": "-",
-    "waktu": "-",
-    "lampu": "OFF"
+"suhu": 0,
+"cahaya": 0,
+"kondisi": "-",
+"waktu": "-",
+"lampu": "OFF"
 }
+
+command = "AUTO"
 
 @app.route("/")
 def home():
 
-    return render_template(
-        "index.html",
-        data=data_sensor
-    )
-
-command = "AUTO"
-
-@app.route("/command", methods=["GET"])
-def get_command():
-
-    return command
-
-@app.route("/set_command/<cmd>")
-def set_command(cmd):
-
-    global command
-
-    command = cmd.upper()
-
-    return jsonify({
-        "status": "success",
-        "command": command
-    })
+```
+return render_template(
+    "index.html",
+    data=data_sensor
+)
+```
 
 @app.route("/data", methods=["POST"])
 def receive_data():
 
-    global data_sensor
+```
+global data_sensor
 
-    data_sensor = request.json
+data_sensor = request.json
 
-    return jsonify({
-        "status": "success"
-    })
+return jsonify({
+    "status": "success"
+})
+```
 
-if __name__ == "__main__":
+@app.route("/sensor")
+def sensor():
 
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+```
+return jsonify(data_sensor)
+```
+
+@app.route("/command")
+def get_command():
+
+```
+return command
+```
+
+@app.route("/set_command/<cmd>")
+def set_command(cmd):
+
+```
+global command
+
+command = cmd.upper()
+
+return jsonify({
+    "status": "success",
+    "command": command
+})
+```
+
+if **name** == "**main**":
+
+```
+app.run(
+    host="0.0.0.0",
+    port=5000,
+    debug=True
+)
+```

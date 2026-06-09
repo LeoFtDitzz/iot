ac0ea03fcc376e72e33b774eb9ef5beaed46de2c
LeoFtDitzz <leoftradit@gmail.com>
2026-06-09 14:16:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 data_sensor = {
-"suhu": 0,
-"cahaya": 0,
-"kondisi": "-",
-"waktu": "-",
-"lampu": "OFF"
+    "suhu": 0,
+    "cahaya": 0
 }
-
-command = "AUTO"
 
 @app.route("/")
 def home():
-return render_template(
-"index.html",
-data=data_sensor
-)
+    return "Hello"
 
 @app.route("/data", methods=["POST"])
 def receive_data():
